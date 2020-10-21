1c548b8d11e68d9ba935945a41a53bf14813952a
Nathan Kimpel <nathan.kimpel@engelvoelkers.com>
2020-10-20 20:08:39 -0500

    Futher testing

---
+++ newFile.js
@@ -0,0 +1,72 @@
+var myName = 'Nathan';
+
+
+console.log(myName);
+//output Nathan
+
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+
+const myName = 'Nathan';
+console.log(myName); //Output Nathan...my name
+const entree = 'Enchiladas';
+console.log(entree);
+
+entree='Tacos';
+
+
+const myName = 'Nathan';
+console.log(myName); //Output Nathan...my name
+const entree = 'Enchiladas';
+console.log(entree);
+
+entree='Tacos';
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+console.log(gainedDollar);
+console.log(lostDollar);
+lostDollar--;
+console.log(lostDollar);
+
+
+let favoriteAnimal = 'cat';
+console.log('My favorite animal: ' + favoriteAnimal); //concatanation is pretty easy
+
+let nathanLearning = 'This is a test';
+//string literals are amazing
+console.log()
+
+let nathanLearning = 'NateDog';
+const TestingAgain = 3;
+console.log(nathanLearning);
+console.log(`Hello whoever you are ${nathanLearning}`);
+
+
